ab432a0a4d269e0397a1f92cd3a1c920c6348fdb
Byson94 <byson94@outlook.com>
2025-06-16 20:29:02 +0530

    updated autogen.py to provide search bar for searching packages

--- autogen.py
+++ autogen.py
@@ -9,45 +9,61 @@ archs = (Path("x86_64"),)
 
 base_url = "https://raw.githubusercontent.com/axos-project/axmirrors/main/{arch}/{package}"
 
+# JavaScript for live search filtering
+search_script = """
+<script>
+function searchPackages(inputId, listId) {
+    const input = document.getElementById(inputId).value.toLowerCase();
+    const listItems = document.getElementById(listId).getElementsByTagName('li');
+    for (let i = 0; i < listItems.length; i++) {
+        const txt = listItems[i].innerText.toLowerCase();
+        listItems[i].style.display = txt.includes(input) ? '' : 'none';
+    }
+}
+</script>
+"""
 
 def main():
     # root index.html content
-    index_text = "<html><head><title>AxOS Packages</title></head><body>"
-    index_text += "<h1>AxOS Packages</h1><ul>"
+    index_text = "<html><head><title>AxOS Packages</title></head><body>\n"
+    index_text += "<h1>AxOS Packages</h1>\n<ul>\n"
 
     for arch in archs:
         # add current arch to root index.html
         logging.info("Generating for arch: %s", arch)
         index_text += f'<li><a href="{arch}/index.html">{arch}</a></li>'
 
-        # this arch index.html content body
-        archs_text = '<html><head><title>Packages for {arch}</title></head><body>'.format(arch=arch)
-        archs_text += '<a href="../index.html">../</a><h1>Packages for {arch}</h1><ul>'.format(arch=arch)
-        
+        # per-arch index.html content
+        arch_text = f"""<html><head><title>Packages for {arch}</title>{search_script}</head><body>
+<a href="../index.html">../</a>
+<h1>Packages for {arch}</h1>
+<input type="text" id="archSearch_{arch}" placeholder="Search..." onkeyup="searchPackages('archSearch_{arch}', 'pkgList_{arch}')">
+<ul id="pkgList_{arch}">
+"""
+
         # get packages list
-        packages = sorted(tuple(arch.glob("*.zst")))
-        
-        # add packages link
+        packages = sorted(arch.glob("*.zst"))
+
         for package in packages:
             logging.info("Generating for package: %s", package)
             _package_url = base_url.format(arch=arch, package=package.name)
-            archs_text += '<li><a href="{url}">{name}</a></li>'.format(
+            arch_text += '<li><a href="{url}">{name}</a></li>'.format(
                 name=package.name.split(".")[0],
                 url=_package_url,
             )
-        archs_text += "</ul></body></html>"
 
-        # create index.html file for this arch
+        arch_text += "</ul></body></html>"
+
+        # write arch-specific index.html
         with open(arch / "index.html", "w") as f:
-            f.write(archs_text)
-    
+            f.write(arch_text)
+
     index_text += "</ul></body></html>"
 
-    # create root index.html file
+    # write root index.html
     with open("index.html", "w") as f:
         f.write(index_text)
 
 
 if __name__ == "__main__":
     main()
-
